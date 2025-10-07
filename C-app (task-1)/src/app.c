dba1f22be22ec673a83a5fff4ff3cac6a87d0438
AlexeyZarechnev <408642@niuitmo.ru>
2025-10-08 01:32:20 +0300

    fix warnings

--- C-app (task-1)/src/app.c
+++ C-app (task-1)/src/app.c
@@ -1,36 +1,37 @@
 #include "sim.h"
 
+#define WIDTH (WINDOW_WIDTH / PIXEL_SCALE)
+#define HEIGHT (WINDOW_HEIGHT / PIXEL_SCALE)
+
 void app(void) {
-  const int width = WINDOW_WIDTH / PIXEL_SCALE;
-  const int height = WINDOW_HEIGHT / PIXEL_SCALE;
-  int cur_state[width + 2][height + 2] = {};
-  int next_state[width + 2][height + 2] = {};
+  int cur_state[WIDTH + 2][HEIGHT + 2] = {};
+  int next_state[WIDTH + 2][HEIGHT + 2] = {};
 
   fill_init_state(cur_state);
 
   for (;;) {
-    for (int i = 1; i < width + 1; ++i) {
-      for (int j = 1; j < height + 1; ++j) {
+    for (int i = 1; i < WIDTH + 1; ++i) {
+      for (int j = 1; j < HEIGHT + 1; ++j) {
         int sum = cur_state[i][j + 1] + cur_state[i][j - 1] +
                   cur_state[i + 1][j] + cur_state[i - 1][j] +
                   cur_state[i - 1][j - 1] + cur_state[i - 1][j + 1] +
                   cur_state[i + 1][j - 1] + cur_state[i + 1][j + 1];
         next_state[i][j] = ((sum == 3) || (cur_state[i][j] && sum == 2));
-        put_pixel(i - 1, height - j, next_state[i][j] ? 0xFFFFFF : 0x0);
+        put_pixel(i - 1, HEIGHT - j, next_state[i][j] ? 0xFFFFFF : 0x0);
       }
     }
     if (flush()) {
       return;
     }
 
-    for (int i = 1; i < width + 1; ++i) {
-      for (int j = 1; j < height + 1; ++j) {
+    for (int i = 1; i < WIDTH + 1; ++i) {
+      for (int j = 1; j < HEIGHT + 1; ++j) {
         int sum = next_state[i][j + 1] + next_state[i][j - 1] +
                   next_state[i + 1][j] + next_state[i - 1][j] +
                   next_state[i - 1][j - 1] + next_state[i - 1][j + 1] +
                   next_state[i + 1][j - 1] + next_state[i + 1][j + 1];
         cur_state[i][j] = ((sum == 3) || (next_state[i][j] && sum == 2));
-        put_pixel(i - 1, height - j, cur_state[i][j] ? 0xFFFFFF : 0x0);
+        put_pixel(i - 1, HEIGHT - j, cur_state[i][j] ? 0xFFFFFF : 0x0);
       }
     }
     if (flush()) {
